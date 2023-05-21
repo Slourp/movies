eefcef2cc90b70e6e6bb64c9c18b3388980cb85c
david vanmak <david.vanmak@pop-france.eu>
2023-05-21 23:33:05 +0200

    [UPDATE] add cron as a shell command

--- app/init.sh
+++ app/init.sh
@@ -1,21 +1,12 @@
 #!/bin/zsh
 
-export VISUAL="nano"
-export EDITOR="nano"
-
-# Install Oh My Zsh
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-
 # Install NVM
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.38.0/install.sh | bash
 
-# Setup environment for NVM
-echo 'export NVM_DIR="$HOME/.nvm"' >> ~/.zshrc
-echo '[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm' >> ~/.zshrc
-echo '[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion" # This loads nvm bash_completion' >> ~/.zshrc
-
-# Source zshrc to load NVM and the ls alias
-source ~/.zshrc
+# Load NVM environment
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
 
 # Install the LTS version of Node.js using NVM
-# nvm install 
+nvm install --lts
